ab14fbb5890f1e94174825f1d6e82fffa2c653d5
Markus Raab <elektra@markus-raab.org>
2019-05-02 14:11:47 +0200

    add java files

---
+++ src/Monom.java
@@ -0,0 +1,51 @@
+// This class represents monoms of the form a*x^d ("a mal x hoch d"), where
+// 'a' is the coefficient of the monom and 'd' is the degree.
+public class Monom {
+
+    //TODO: declare variables
+
+    // A constructor with the coefficient 'coeff' and the 'degree' of this monom.
+    public Monom(int coeff, int degree) {
+        // TODO: implement this constructor
+    }
+
+    // Copy-constructor: uses 'coeff' and 'degree' of 'm' to initialize this monom.
+    public Monom(Monom m) {
+        // TODO: implement this constructor
+    }
+
+    // Adds the monom 'm' to this monom, if both monoms have the same degree.
+    // In this case this polynom's coefficient is replaced by the sum of this polynom's
+    // coefficient and the coefficient of 'm'. In this case the method returns 'true'.
+    // If 'm' has not the same degree as 'this', the method has no effect and
+    // returns 'false'.
+    public boolean combine(Monom m) {
+        // TODO: implement this method
+        return false;
+    }
+
+    // Returns 'true' if 'm' has a higher degree than 'this', and 'false' otherwise.
+    public boolean lowerDegreeThan(Monom m) {
+        // TODO: implement this method
+        return false;
+    }
+
+    // Returns the value of the monom for a specified value of 'x'
+    public int eval(int x) {
+        // TODO: implement this method
+        return 0;
+    }
+
+
+    // Returns a representation in mathematical notation, e.g. of the form "5*x^2".
+    public String toString() {
+        // TODO: implement this method
+        return "";
+    }
+
+
+}
+
+
+
+
---
+++ src/TestApplication.java
@@ -0,0 +1,35 @@
+/*
+This class can be modified and is for testing your solution.
+Modifications will NOT be reviewed or assessed.
+*/
+public class TestApplication {
+
+    public static void main(String[] args) {
+
+        Monom m = new Monom(3,2);
+        System.out.println(m); // 3*x^2
+        System.out.println(m.eval(-5)); // 75
+        System.out.println(m.eval(0)); // 0
+
+        Polynom p1 = new Polynom(new int[] {2,0,3,1});
+        Polynom p2 = new Polynom(new int[] {0,0,3,-1});
+
+        System.out.println(p1); // 2*x^0 + 3*x^2 + 1*x^3
+        System.out.println(p1.eval(-5)); // -48
+        System.out.println(p2); // 3*x^2 + -1*x^3
+
+        p1.add(p2);
+
+        System.out.println(p1); // 2*x^0 + 6*x^2 + 0*x^3
+
+
+        p1 = new Polynom(new int[] {2,0,3,1});
+        p2 = new Polynom(new int[] {0,0,3,1});
+
+        p2.add(p1);
+
+        System.out.println(p2); // 2*x^0 + 6*x^2 + 2*x^3
+
+    }
+}
+
